775157417476909bdaf93da15bd10f385945bfab
pennipop <kondanai@gmail.com>
2025-11-19 17:17:13 -0500

    level changes

--- BLOCK PLATFORMER/Assets/Resources/Testing/Scripts/PlayerController.cs
+++ BLOCK PLATFORMER/Assets/Resources/Testing/Scripts/PlayerController.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
-    [SerializeField] float jumpForce;
+    [SerializeField] float baseMoveSpeed;
+    [SerializeField] float baseJumpForce;
+
+    float moveSpeed;
+    float jumpForce;
 
     Vector2 dir;
     Transform grabbableBox;
@@ -22,6 +25,9 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         hand = transform.Find("Hand");
+
+        moveSpeed = baseMoveSpeed;
+        jumpForce = baseJumpForce;
     }
 
     // Update is called once per frame
@@ -87,11 +93,13 @@ public class PlayerController : MonoBehaviour
             hand.GetChild(0).rotation = grabbableBox.rotation;
             hand.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
 
-            if (Input.GetKey(KeyCode.LeftShift))
+            if (Input.GetKey(KeyCode.J))
             {
                 heldBox = grabbableBox;
                 hand.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
                 grabbableBox = null;
+                moveSpeed = baseMoveSpeed / 2;
+                jumpForce = baseJumpForce / 1.25f;
             }
         }
         else
@@ -107,11 +115,13 @@ public class PlayerController : MonoBehaviour
         heldBox.position = hand.position;
         heldBox.GetComponent<Rigidbody2D>().gravityScale = 0;
 
-        if (Input.GetKeyUp(KeyCode.LeftShift))
+        if (Input.GetKeyUp(KeyCode.J))
         {
             heldBox.GetComponent<Collider2D>().enabled = true;
             heldBox.GetComponent<Rigidbody2D>().gravityScale = 2;
             heldBox.GetComponent<Rigidbody2D>().AddForce(new Vector2(dir.x, dir.y + 1) * throwForce, ForceMode2D.Impulse);
+            moveSpeed = baseMoveSpeed;
+            jumpForce = baseJumpForce;
             heldBox = null;
         }
     }
